91b122d27e29c2476a12f2f808b9cdf4c73cfb95
zerovolk <sozikin.artyom@yandex.ru>
2022-07-06 01:21:48 +0500

    Added Scss to mainFooter

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -30,60 +30,61 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer {
   background: #EBEBEC;
   width: 100%;
   height: 130px;
   position: relative;
-}
-.footer__nav {
-  display: block;
-  text-align: center;
-}
 
-.footer__nav ul {
-  list-style: none;
-}
+  .footer__nav {
+    display: block;
+    text-align: center;
 
-.footer__item {
-  display: inline-block;
-  margin: 22px 18px 14px 18px;
-  cursor: pointer;
-}
+    ul {
+      list-style: none;
 
-.footer__sublime {
-  width: 60%;
-  height: 0;
-  border: 1px solid #5F5F61;
-  position: absolute;
-  left: calc(50% - 1180px/2);
-  top: calc(50% - 1px/2 + 0.5px);
-}
+      .footer__item {
+        display: inline-block;
+        margin: 22px 18px 14px 18px;
+        cursor: pointer;
+      }
+    }
+  }
 
-.footer__social {
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  margin-top: 20px;
-}
+  .footer__sublime {
+    width: 60%;
+    height: 0;
+    border: 1px solid #5F5F61;
+    position: absolute;
+    left: calc(50% - 1180px/2);
+    top: calc(50% - 1px/2 + 0.5px);
+  }
 
-.footer__icon {
-  text-align: center;
-  width: 24px;
-  height: 24px;
-  background: #5F5F61;
-  border-radius: 50%;
-  color: #FFFBF9;
-  line-height: 24px;
-  margin: 0 18px;
-  cursor: pointer;
-}
+  .footer__social {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    margin-top: 20px;
 
-.footer__text {
-  text-align: center;
-  margin-top: 8px;
-  font-size: 14px;
-  color: #5F5F61;
+    .footer__icon {
+      text-align: center;
+      width: 24px;
+      height: 24px;
+      background: #5F5F61;
+      border-radius: 50%;
+      color: #FFFBF9;
+      line-height: 24px;
+      margin: 0 18px;
+      cursor: pointer;
+    }
+  }
+
+  .footer__text {
+    text-align: center;
+    margin-top: 8px;
+    font-size: 14px;
+    color: #5F5F61;
+  }
 }
 </style>
